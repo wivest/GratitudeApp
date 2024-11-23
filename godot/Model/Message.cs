df8fa231e6b9f07634739039cdd83b287413c839
wivest <120594169+wivest@users.noreply.github.com>
2024-11-23 17:54:49 +0100

    feat: extend Message properties

--- godot/Model/Message.cs
+++ godot/Model/Message.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.Collections;
 
 namespace GratitudeApp.Model;
 
@@ -6,4 +7,13 @@ public partial class Message : Resource
 {
     [Export]
     public string Text { get; set; }
+
+    [Export]
+    public Array<Person> Tagged { get; set; }
+
+    [Export]
+    public float Time { get; set; }
+
+    [Export]
+    public bool Edited { get; set; }
 }
