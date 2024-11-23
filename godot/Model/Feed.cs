422b253963da40e414c309f728cb5d3c95768055
wivest <120594169+wivest@users.noreply.github.com>
2024-11-23 18:27:38 +0100

    feat: create Feed class

---
+++ godot/Model/Feed.cs
@@ -0,0 +1,20 @@
+using Godot;
+
+namespace GratitudeApp.Model;
+
+public partial class Feed : ScrollContainer
+{
+    [Export]
+    private InputLine inputLine;
+
+    private VBoxContainer container;
+
+    public override void _Ready()
+    {
+        container = GetNode<VBoxContainer>("Container");
+
+        inputLine.MessageSaved += OnNoteSaved;
+    }
+
+    private void OnNoteSaved(string note) { }
+}
