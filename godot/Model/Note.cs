a9fc6401ab3525780ad4f17a06ab66da6e377a91
wivest <120594169+wivest@users.noreply.github.com>
2024-11-25 23:15:50 +0100

    feat: create readonly attribute

--- godot/Model/Note.cs
+++ godot/Model/Note.cs
@@ -4,12 +4,16 @@ namespace GratitudeApp.Model;
 
 public partial class Note : VBoxContainer
 {
+    public readonly NoteData NoteResource;
+
     private Label text;
     private Label date;
     private Label edited;
 
     public Note(NoteData note)
     {
+        NoteResource = note;
+
         text = new Label { Text = note.Text };
         date = new Label { Text = note.TimeString };
         edited = new Label { Text = note.EditedString };
