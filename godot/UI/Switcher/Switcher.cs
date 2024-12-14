e89b9ba93797cc7bccdb678c76ca7251e9a8251e
wivest <120594169+wivest@users.noreply.github.com>
2024-12-14 23:32:23 +0100

    feat: use Switcher as container for node

--- godot/UI/Switcher/Switcher.cs
+++ godot/UI/Switcher/Switcher.cs
@@ -3,8 +3,23 @@ using Godot.Collections;
 
 namespace GratitudeApp.UI.Switcher;
 
-public partial class Switcher : HBoxContainer
+public partial class Switcher : VBoxContainer
 {
     [Export]
     private Array<Node> States;
+
+    private Node stateNode;
+    private HBoxContainer buttons;
+
+    public override void _Ready()
+    {
+        stateNode = GetNode<Node>("State");
+        buttons = GetNode<HBoxContainer>("Buttons");
+
+        foreach (Node state in States)
+        {
+            var button = new Button { Text = state.Name };
+            buttons.AddChild(button);
+        }
+    }
 }
