e907a80513dfd06616df8614157dc9a1eeb44f17
wivest <120594169+wivest@users.noreply.github.com>
2024-12-23 14:44:28 +0100

    feat: implement container in Feed

--- godot/UI/People/Feed/Feed.cs
+++ godot/UI/People/Feed/Feed.cs
@@ -1,4 +1,6 @@
 using Godot;
+using GratitudeApp.Model;
+using GratitudeApp.Model.Save;
 
 namespace GratitudeApp.UI.People;
 
@@ -6,4 +8,21 @@ public partial class Feed : ScrollContainer
 {
     [Export]
     private InputLine inputLine;
+
+    private VBoxContainer container;
+
+    private readonly Saver<PersonSave> saver = new("user://people.tres");
+
+    public override void _Ready()
+    {
+        container = GetNode<VBoxContainer>("Container");
+        LoadPeople();
+    }
+
+    private void LoadPeople()
+    {
+        PersonSave people = saver.Load();
+        foreach (Person person in people.People)
+            container.AddChild(new PersonHint(person));
+    }
 }
