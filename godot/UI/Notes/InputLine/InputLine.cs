ef86b0581da86483154382cb02387a3863bdfbef
wivest <120594169+wivest@users.noreply.github.com>
2024-12-26 23:39:25 +0100

    feat: load people data

--- godot/UI/Notes/InputLine/InputLine.cs
+++ godot/UI/Notes/InputLine/InputLine.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Godot;
 using GratitudeApp.Model;
+using GratitudeApp.Model.Save;
 
 namespace GratitudeApp.UI.Notes;
 
@@ -11,6 +12,8 @@ public partial class InputLine : Control
 
     private LineEdit lineEdit;
     private Button saveButton;
+
+    private readonly Saver<PeopleSave> saver = new("user://people.tres");
 
     public override void _Ready()
     {
@@ -29,6 +32,8 @@ public partial class InputLine : Control
         string tag = lastWord.Split('@')[^1];
 
         // TODO: suggest matchings
+        PeopleSave save = saver.Load();
+        var people = new List<PersonData>(save.People);
     }
 
     private void OnMessageSaved()
