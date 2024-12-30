dfb3cb16bb73ed7ec5d0272ec6a8236b6d1db2d5
wivest <120594169+wivest@users.noreply.github.com>
2024-12-30 22:24:30 +0100

    feat: add hints to container

--- godot/UI/Notes/Hints/Hints.cs
+++ godot/UI/Notes/Hints/Hints.cs
@@ -1,5 +1,24 @@
+using System;
 using Godot;
+using GratitudeApp.Model;
 
 namespace GratitudeApp.UI.Notes;
 
-public partial class Hints : VBoxContainer { }
+public partial class Hints : VBoxContainer
+{
+    public event Action<Person> PersonChosen;
+
+    [Export]
+    private InputLine inputLine;
+
+    public override void _Ready()
+    {
+        inputLine.PersonMatched += OnPersonMatched;
+    }
+
+    private void OnPersonMatched(PersonData person)
+    {
+        var hint = new Button { Text = $"{person.Name} ({person.Tag})" };
+        AddChild(hint);
+    }
+}
